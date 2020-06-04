eb9fac4e321703266c22993803a700c2fdd0f766
Sidnei - Inventti <sidnei@inventti.com.br>
2020-06-04 19:20:36 -0300

    autoload hcodebr

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit3c84d73f5d8fb32e513c945761cec54c
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'TopSolution\\' => 12,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'TopSolution\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit3c84d73f5d8fb32e513c945761cec54c
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit3c84d73f5d8fb32e513c945761cec54c::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit3c84d73f5d8fb32e513c945761cec54c::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit3c84d73f5d8fb32e513c945761cec54c::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit3c84d73f5d8fb32e513c945761cec54c::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit3c84d73f5d8fb32e513c945761cec54c::$classMap;
 
