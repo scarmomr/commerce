55da21542cf823f2da3bae1ba9a74622ea38da7f
Sidnei - Inventti <sidnei@inventti.com.br>
2020-06-06 11:34:40 -0300

    implementação tela de login

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \TopSolution\Page;
+use \TopSolution\PageAdmin;
+use \TopSolution\Model\User;
 
 $app = new Slim();
 
@@ -16,6 +20,45 @@ $app->get('/', function() {
    $page->setTpl("index");
 
 });
+
+$app->get('/admin', function() {
+   
+	User::verifyLogin();
+
+   $page = new PageAdmin();
+   
+   $page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+   
+   $page = new PageAdmin([
+   	"header"=>false,
+   	"footer"=>false
+   ]);
+   
+   $page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() { 
+
+User::logout();
+
+header("Location: /admin/login");
+exit;
+
+}); 
 
 $app->run();
 
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,78 @@
+<?php 
+
+namespace TopSolution\Model;
+use \TopSolution\DB\SQL;
+use \TopSolution\Model;
+
+class User extends Model {
+
+	const SESSION = "User";
+
+	protected $fields = [
+		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister"
+	];
+
+	public static function login($login, $password):User
+	{
+
+		$db = new Sql();
+
+		$results = $db->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+			":LOGIN"=>$login
+		));
+
+		if (count($results) === 0) {
+			throw new \Exception("Não foi possível fazer login.");
+		}
+
+		$data = $results[0];
+
+		if (password_verify($password, $data["despassword"])) {
+
+			$user = new User();
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
+
+		} else {
+
+			throw new \Exception("Não foi possível fazer login.");
+
+		}
+
+	}
+
+	public static function logout()
+	{
+
+		$_SESSION[User::SESSION] = NULL;
+
+	}
+
+	public static function verifyLogin($inadmin = true)
+	{
+
+		if (
+			!isset($_SESSION[User::SESSION])
+			|| 
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			$_SESSION[User::SESSION]["iduser"] == ""
+			||
+			(bool)$_SESSION[User::SESSION]["iduser"] !== $inadmin
+		) {
+			
+			header("Location: /admin/login");
+			exit;
+
+		}
+
+	}
+
+}
+
+ ?>
