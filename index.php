0db5f4e96fa8128d03153750375e310b585fce3f
Sidnei - Inventti <sidnei@inventti.com.br>
2020-06-07 12:30:59 -0300

    Implementaçao Crud - Somente inserção

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
    
-	User::verifyLogin();
+   User::verifyLogin();
 
    $page = new PageAdmin();
    
@@ -59,6 +59,69 @@ header("Location: /admin/login");
 exit;
 
 }); 
+
+$app->get("/admin/users", function() {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+$app->get("/admin/users/create", function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser) {
+
+	User::verifyLogin();
+
+});
+
+$app->get("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("users-update");
+
+});
+
+$app->post("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->setData($_POST);
+	
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+	User::verifyLogin();
+
+});
+
 
 $app->run();
 
